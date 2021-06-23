e436a20e70af2da29b3e4053e528aa15037b9db0
muktar islam <muktarislam95@gmail.com>
2021-06-23 23:01:30 +0600

    emajohn is initialize

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import React, { useState } from 'react';
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const handleIncrease = () => {
+//     setCount (count + 1)
+//   };
+//   const handleDecrease = () => {
+//     setCount (count - 1)
+//   };
+//   const handleMultiplacation = () => {
+//     setCount (count * (1+300))
+//   };
+//   const handleAC = () => {
+//     setCount (count % 1)
+//   };
+//     return (
+//       <div style={{textAlign: "center"}}>
+//         <h2>Count : {count}</h2>
+//         <button onClick={handleIncrease}>Increase</button>
+//         <button onClick={handleDecrease}>Decrease</button>
+//         <button onClick={handleMultiplacation}>Multiplacation</button>
+//         <button onClick={handleAC}>AC</button>
+//       </div>
+//     );
+// }
+// export default App;
